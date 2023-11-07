01ccdc93311d5f162cf5281c69b764b09c8a5e72
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-11-07 16:46:53 -0300

    fix: layout

--- components/QrCard.tsx
+++ components/QrCard.tsx
@@ -2,22 +2,24 @@ import Image from "next/image";
 
 export default function QrCard() {
 	return (
-		<div className="w-80 bg-white rounded-xl p-4 flex flex-col justify-center items-center shadow-2xl shadow-darkBlue/20">
+		<div className="w-[22rem] bg-white rounded-2xl p-4 flex flex-col justify-center items-center shadow-2xl shadow-darkBlue/20">
 			<div className="flex flex-col justify-center items-center text-center space-y-4">
 				<Image
 					className="rounded-xl"
 					src="/image-qr-code.png"
 					alt="QR Code"
-					width={300}
-					height={300}
+					width={400}
+					height={400}
 				/>
-				<h2 className="font-bold text-2xl">
-					Improve your front-end skills by building projects
-				</h2>
-				<p className="text-grayishBlue pb-4">
-					Scan the QR code to visit Frontend Mentor and take your
-					coding skills to the next level
-				</p>
+				<div className="px-6 space-y-4">
+					<h2 className="font-semibold text-2xl">
+						Improve your front-end skills by building projects
+					</h2>
+					<p className="text-grayishBlue pb-6">
+						Scan the QR code to visit Frontend Mentor and take your
+						coding skills to the next level
+					</p>
+				</div>
 			</div>
 		</div>
 	);
